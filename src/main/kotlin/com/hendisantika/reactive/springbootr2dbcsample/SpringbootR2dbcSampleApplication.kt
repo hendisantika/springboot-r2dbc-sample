4e14c133aeedf5c17c4cab5caf72880b21f6938c
hendisantika <hendisantika@yahoo.co.id>
2019-02-23 06:08:27 +0700

    #1 - Initial commit

---
+++ src/main/kotlin/com/hendisantika/reactive/springbootr2dbcsample/SpringbootR2dbcSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.reactive.springbootr2dbcsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootR2dbcSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootR2dbcSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/hendisantika/reactive/springbootr2dbcsample/SpringbootR2dbcSampleApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.hendisantika.reactive.springbootr2dbcsample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringbootR2dbcSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
